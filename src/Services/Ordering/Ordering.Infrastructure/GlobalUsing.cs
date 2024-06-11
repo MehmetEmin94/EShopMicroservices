982c9d0c30a295194b4215098a695deecae3d798
MehmetEmin94 <emin.aykan.ea@gmail.com>
2024-06-11 13:28:10 +0300

    Configuration classes created

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -1,1 +1,5 @@
-﻿
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
