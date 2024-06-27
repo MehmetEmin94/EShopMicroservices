eaf739e84e09f9a65950aa48004fc2010575b62f
MehmetEmin94 <emin.aykan.ea@gmail.com>
2024-06-27 12:59:23 +0300

    DeleteOrder Commands

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderHandler(IApplicationDbContext dbContext)
+        : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            //check entity is exist
+            var orderId = OrderId.Of(command.Id);
+
+            var order = await dbContext.Orders
+                .FindAsync([orderId], cancellationToken: cancellationToken);
+
+            if (order is null)
+            {
+                throw new OrderNotFoundException(command.Id);
+            }
+
+            //delete entity
+            dbContext.Orders.Remove(order);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return new DeleteOrderResult(true);
+        }
+    }
+}
