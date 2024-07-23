110ae0f5c9ce59190f1d9de5d8319cc84be44303
MehmetEmin94 <emin.aykan.ea@gmail.com>
2024-07-23 12:48:11 +0300

    Messaging events classes created

--- src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BuildingBlocks.Messaging.Events
+﻿namespace BuildingBlocks.Messaging.Events
 {
-    internal class IntegrationEvent
+    public record IntegrationEvent
     {
+        public Guid Id => Guid.NewGuid();
+        public DateTime OccuredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName;
     }
 }
