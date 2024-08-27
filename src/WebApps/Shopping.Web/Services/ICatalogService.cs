bda93f907c23150e3fc01dc7680d9322c34aadaa
MehmetEmin94 <emin.aykan.ea@gmail.com>
2024-08-27 14:57:53 +0300

    Refit HttpClientFactory added

---
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Shopping.Web.Services
+{
+    public interface ICatalogService
+    {
+        Task<GetProductsResponse> GetProducts(int? PageNumber = 1, int? PageSize = 10);
+        Task<GetProductByIdResponse> GetProduct(Guid id);
+        Task<GetProductsByCategoryResponse> GetProductsByCategory(string category);
+    }
+}
